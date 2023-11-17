6f0a81749ca148db2976cb840597b2c6ca934f62
gabecodes2 <gabrielcastanheira2002@outlook.com>
2023-11-16 20:40:31 -0500

    brewery cards fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,49 +2,44 @@ var temp = document.getElementById('temperature0');
 var cityname = document.getElementById('city0');
 var searchform = document.querySelector('.search');
 var cityinput = document.getElementById('cityInput');
-var brewerycontainer = document.getElementById('brewery-container');
+var brewerycontainer = document.getElementById('brewery-container'); 
 var apiKey = "fcdb77bcab0f5e656f374a185e3665bd";
 
-
-function getWeather(lat,lon) {
+function getWeather(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
-      .then(response => response.json())
-      .then(data=> displayWeather(data))
-    
+        .then(response => response.json())
+        .then(data => displayWeather(data));
 }
+
 function getCoordinates(city) {
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log("Weather API Response:", data);
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-        getWeather(lat, lon);
-        getBrewery(city);
-    })
-    .catch(error => console.error("Error fetching weather API:", error));
+        .then(response => response.json())
+        .then(data => {
+            console.log("Weather API Response:", data);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            getWeather(lat, lon);
+            getBrewery(city);
+        })
+        .catch(error => console.error("Error fetching weather API:", error));
 }
 
 function getBrewery(city) {
     fetch(`https://api.openbrewerydb.org/v1/breweries?by_city=${city}&per_page=3`)
-    .then(response => response.json())
-    .then(data => {
-        console.log("Brewery API Response:", data);
-        displayBrewery(data);
-    })
-    .catch(error => console.error("Error fetching brewery API:", error));
+        .then(response => response.json())
+        .then(data => {
+            console.log("Brewery API Response:", data);
+            displayBrewery(data);
+        })
+        .catch(error => console.error("Error fetching brewery API:", error));
 }
 
-
-
-
-
-function displayWeather(data){
-    console.log(data)
-    cityname.textContent=data.name
-    temp.textContent= data.main.temp
-    var iconCode= data.weather[0].icon;
-    var iconImg= document.createElement('img');
+function displayWeather(data) {
+    console.log(data);
+    cityname.textContent = data.name;
+    temp.textContent = data.main.temp;
+    var iconCode = data.weather[0].icon;
+    var iconImg = document.createElement('img');
     iconImg.src = `http://openweathermap.org/img/w/${iconCode}.png`;
     temp.appendChild(iconImg);
 }
@@ -52,12 +47,9 @@ function displayWeather(data){
 function displayBrewery(breweryData) {
     brewerycontainer.innerHTML = "";
 
-    var tileContainer = document.createElement('div');
-    tileContainer.classList.add('tile');
-
     breweryData.forEach(brewery => {
         var breweryTile = document.createElement('article');
-        breweryTile.classList.add('tile', 'is-child', 'notification', 'is-primary');
+        breweryTile.classList.add('tile', 'is-child', 'box'); 
 
         var breweryName = document.createElement('p');
         breweryName.classList.add('title');
@@ -69,19 +61,13 @@ function displayBrewery(breweryData) {
 
         breweryTile.appendChild(breweryName);
         breweryTile.appendChild(breweryAddress);
-        tileContainer.appendChild(breweryTile);
+        brewerycontainer.appendChild(breweryTile);
     });
-
-    brewerycontainer.appendChild(tileContainer);
 }
 
-  
-
-
-
-searchform.addEventListener("submit", (event) =>{
-    event.preventDefault()
-    console.log(cityinput.value)
-getCoordinates(cityinput.value)
-getBrewery(cityinput.value)
-} )
+searchform.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(cityinput.value);
+    getCoordinates(cityinput.value);
+    getBrewery(cityinput.value);
+});
